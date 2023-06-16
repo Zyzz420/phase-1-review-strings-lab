6f5abf77457becf98d63b826a8b1402cd6914a2a
Abdulaziz <abdulaziz.komara@student.moringaschool.com>
2023-06-16 09:51:25 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser='currentUser1'
+const welcomeMessage="Welcome to Flatbook, 'currentuser'!"
+const  excitedWelcomeMessage ="WELCOME TO FLATBOOK, 'currentUser1'!"
+const shortGreeting="Welcome, c!"
